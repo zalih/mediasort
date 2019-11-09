77e49335db098acc2dc33f748009e538c5254314
Salih Kiliclioglu <salih.1975@hotmail.com>
2019-11-10 00:56:24 +0100

    APPS-3 support multiple datetime formats in filename

--- mediasort.py
+++ mediasort.py
@@ -19,32 +19,34 @@ import logging
 
 # global variables
 
-# available formats used for output folder creation
-output_formats = {
+output_formats = {  # available formats used for output folder creation
     'YEARLY': "%Y",
     'MONTHLY': "%Y-%m",
     'DAILY': "%Y-%m-%d-%a"
 }
-# variable to store output folder format
-output_format = ""
 
-EXIF_DATETIME = "%Y:%m:%d %H:%M:%S"
-FILENAME_DATETIME = "%Y%m%d_%H%M%S"
+DATETIME_FORMAT_EXIF = "%Y:%m:%d %H:%M:%S"
+
+datetime_formats = {
+    'IOS': {'regex': r'\d{8}_\d{6}', 'datetime': '%Y%m%d_%H%M%S'},
+    'OTHER': {'regex': r'\d{8}\-\d{6}', 'datetime': '%Y%m%d-%H%M%S'}
+}
+
+FILE_EXTENSION_EXIF = 'JPG|jpg|jpeg|BMP|bmp|PNG|png'
+FILE_EXTENSION_VIDEO = 'MPG|mpg|MOV|mov|AVI|avi|MPEG|mpeg|MP4|mp4'
+FILE_EXTENSION_OTHER = 'tiff|tif|TIF'
+
 DEFAULT_SOURCE_FOLDER = "."
 DEFAULT_TARGET_FOLDER = "."
 
 # subfolder for video files
 DEFAULT_VIDEO_FOLDER = "video"
 
-filename_pattern = FILENAME_DATETIME
-video_subfolder = DEFAULT_VIDEO_FOLDER
-
 # variables to store command line options
 opt_recursion = None
 recursion_level = 0  # type: int
-opt_simulate = True
+opt_simulate = False
 opt_single = True
-loglevel = ""
 
 
 def get_field(exif, field):
@@ -53,12 +55,10 @@ def get_field(exif, field):
             return v
 
 
-def make_foldername(date_str, pattern):
-    global output_format
-    return make_foldername(date_str, pattern, output_format)
-
-
-def make_foldername(date_str, pattern, output_pattern):
+# date_str = input string
+# pattern = source date format to parse date from input
+# output_pattern = target date format for output
+def make_foldername_from_date(date_str, pattern, output_pattern):
     if date_str:
         image_datetime = datetime.strptime(date_str, pattern)
         date_str = image_datetime.strftime(output_pattern)
@@ -66,9 +66,9 @@ def make_foldername(date_str, pattern, output_pattern):
     return date_str
 
 
-def get_date_from_filename(name):
+def get_date_from_filename(param_name, datetime_format):
     date_str = None
-    filename_matches = re.search(r"(\d{8}_\d{6})", name)
+    filename_matches = re.search(r"%s" % datetime_format['regex'], param_name)
     if filename_matches:
         date_str = filename_matches.group()
     return date_str
@@ -76,39 +76,44 @@ def get_date_from_filename(name):
 
 def get_date_from_exif(exif_data):
     date_str = get_field(exif_data, 'DateTime')
-    return make_foldername(date_str, EXIF_DATETIME)
+    return date_str
 
 
 def create_target_folder(path):
     if not os.path.exists(path):
         if not opt_simulate:
-            None
-            # TODO: activate after successful simulation
-            # os.makedirs(os.path.join(path))
+            os.makedirs(os.path.join(path))
         logging.debug("Folder created: " + path)
     return
 
 
 def move_file(filename, source_folder, target_folder, subfolder_name, video=""):
+    moved = False
     create_target_folder(os.path.join(target_folder, subfolder_name, video))
 
     if not opt_simulate:
-        # TODO: activate after successful simulation
-        # shutil.move(os.path.join(source_folder, filename),
-        #            os.path.join(target_folder, video, filename))
-        logging.info(
-            os.path.join(source_folder, filename) + ": moving to " +
-            os.path.join(target_folder, subfolder_name, video, filename))
+        try:
+            shutil.move(os.path.join(source_folder, filename),
+                        os.path.join(target_folder, subfolder_name, video))
+            logging.info(
+                os.path.join(source_folder, filename) + ": moving to " +
+                os.path.join(target_folder, subfolder_name, video))
+        except IOError:
+            moved = False
+        else:
+            moved = True
     else:
         logging.info("Simulation: " +
                      os.path.join(source_folder, filename) + ": moving to " +
-                     os.path.join(target_folder, subfolder_name, video, filename))
+                     os.path.join(target_folder, subfolder_name, video))
+    return moved
 
 
-def move_video_file(filename, source, target):
+def move_video_file(filename, source, target, output_format, datetime_format):
     global DEFAULT_VIDEO_FOLDER
     logging.debug("Video file: " + filename)
-    subfolder_name = get_date_from_filename(filename)
+    subfolder_name = make_foldername_from_date(get_date_from_filename(filename, datetime_format),
+                                               datetime_format['datetime'], output_format)
     if subfolder_name:
         move_file(filename, source, target, subfolder_name, DEFAULT_VIDEO_FOLDER)
     else:
@@ -116,9 +121,10 @@ def move_video_file(filename, source, target):
     return
 
 
-def move_other_file(filename, source, output):
+def move_other_file(filename, source, output, datetime_format):
     logging.debug("Other file: " + filename)
-    subfolder_name = make_foldername(get_date_from_filename(filename), FILENAME_DATETIME)
+    subfolder_name = make_foldername_from_date(get_date_from_filename(filename, datetime_format),
+                                               datetime_format['regex'], output_format)
     if subfolder_name:
         move_file(filename, source, output, subfolder_name)
     else:
@@ -126,18 +132,15 @@ def move_other_file(filename, source, output):
     return
 
 
-def move_exif_file(exif_file, filename, source, output):
+def move_exif_file(exif_file, filename, source, output, output_format):
+    moved = False
     logging.debug("EXIF file: " + filename)
     exif_data = exif_file._getexif()
     if exif_data:
-        subfolder_name = get_date_from_exif(exif_data)
+        subfolder_name = make_foldername_from_date(get_date_from_exif(exif_data), DATETIME_FORMAT_EXIF, output_format)
         if subfolder_name:
-            move_file(filename, source, output, subfolder_name)
-        else:
-            move_other_file(filename, source, output)
-    else:
-        move_other_file(filename, source, output)
-    return
+            move = move_file(filename, source, output, subfolder_name)
+    return moved
 
 
 def check_match(regex_pattern, str):
@@ -151,21 +154,16 @@ def check_match(regex_pattern, str):
     return match
 
 
-def scan_files(source, output, max_recursion_level):
+def scan_files(source, output, max_recursion_level, output_format):
     global recursion_level
-    global loglevel
-
-    filename_regex = r"(?P<filename>.*)\.(?P<extension>JPG|jpg|jpeg|BMP|bmp)"
-    filename_regex_video = r"(?P<filename>.*)\.(?P<extension>MPG|mpg|MOV|mov|AVI|avi|MPEG|mpeg|MP4|mp4)"
-    filename_regex_other = r"(?P<filename>.*)\.(?P<extension>tiff|tif|TIF|PNG|png)"
 
     # increase global recursion counter when function entered and decrease before exit
     recursion_level += 1
-    logging.debug("Recursion Level: " + str(recursion_level) + "/" + str(opt_recursion))
+    logging.debug("Recursion Level: " + str(recursion_level) + "/" + str(max_recursion_level))
 
     for filename in os.listdir(source):
         logging.debug("Checking: " + filename)
-        if os.path.isdir(os.path.join(source, filename)) and opt_recursion:
+        if os.path.isdir(os.path.join(source, filename)) and max_recursion_level:
             logging.debug(
                 filename + " is a folder. " + "Max level " + str(max_recursion_level) + " Current level " + str(
                     recursion_level))
@@ -174,28 +172,39 @@ def scan_files(source, output, max_recursion_level):
                 # if no output folder specified, create output folder in the same location as the source file
                 if not opt_single:
                     output = os.path.join(source, filename)
-                scan_files(os.path.join(source, filename), output, recursion_level)
+                scan_files(os.path.join(source, filename), output, max_recursion_level, output_format)
 
-        if check_match(filename_regex, filename):
-            with open(os.path.join(source, filename), 'rb') as file:
-                image_file = Image.open(file)
-                move_exif_file(image_file, filename, source, output)
+        if check_match(r"(?P<filename>.*)\.(?P<extension>%s)" % FILE_EXTENSION_EXIF, filename):
+            with open(os.path.join(source, filename), 'rb') as exif_file:
+                image_file = Image.open(exif_file)
+                move_exif_file(image_file, filename, source, output, output_format)
 
-        if check_match(filename_regex_video, filename):
-            move_video_file(filename, source, output)
+        match = False
+        for datetime_format in datetime_formats:
+            if check_match(r"(?P<filename>%s)\.(?P<extension>%s)" % (datetime_formats[datetime_format]['regex'], FILE_EXTENSION_VIDEO), filename):
+                move_video_file(filename, source, output, output_format, datetime_formats[datetime_format])
+                match = True
+                break
+            elif check_match(r"(?P<filename>%s)\.(?P<extension>%s)" % (datetime_formats[datetime_format]['regex'], FILE_EXTENSION_OTHER),
+                             filename):
+                move_other_file(filename, source, output, output_format, datetime_formats[datetime_format])
+                match = True
+                break
+            elif os.path.isdir(os.path.join(source, filename)):
+                logging.debug(os.path.join(source, filename) + ": Is a directory ")
+                match = True
 
-        if check_match(filename_regex_other, filename):
-            move_other_file(filename, source, output)
-    recursion_level -= 1
+        if not match:
+            logging.warning(os.path.join(source, filename) + ": Can't get date from exif or filename ")
+
+        recursion_level -= 1
     return
+
 
 def main(argv):
     global opt_recursion
     global opt_simulate
     global opt_single
-    global loglevel
-    global output_format
-    global filename_pattern
     global DEFAULT_SOURCE_FOLDER
     global DEFAULT_TARGET_FOLDER
 
@@ -243,7 +252,7 @@ def main(argv):
         logging.debug("Recursion:" + str(opt_recursion))
 
     # start reading the source folder
-    scan_files(source_folder, output_folder, opt_recursion)
+    scan_files(source_folder, output_folder, opt_recursion, output_format)
 
 
 if __name__ == "__main__":
@@ -251,7 +260,6 @@ if __name__ == "__main__":
                         format='%(asctime)s %(levelname)s %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S',
                         level=logging.INFO)
-
 
     logging.info("mediasort.py started")
     main(sys.argv[1:])
--- test_mediasort.py
+++ test_mediasort.py
@@ -1,20 +1,100 @@
 import unittest
-from mediasort import get_date_from_filename
-from mediasort import make_foldername, FILENAME_DATETIME, output_formats
+import testdata
+import mediasort
+# import atexit
+import os
+import logging
+import inspect
+
+from mediasort import get_date_from_filename, make_foldername_from_date
+from mediasort import output_formats, datetime_formats
 
 
+# atexit.register(testdata.cleanup_test_data)
 
-class MyTestCase(unittest.TestCase):
+
+class MediasortOutput(unittest.TestCase):
+    def log_testcase_name(self, param_name):
+        logging.info("****** TC: " + param_name + " ******")
+
+    # unit tests
     def test_get_date_from_filename(self):
-        self.assertEqual(get_date_from_filename("file-20190222_153422.mp4"), "20190222_153422")
-        self.assertEqual(get_date_from_filename("20190222_153422.mp4"), "20190222_153422")
-        self.assertEqual(get_date_from_filename("20190222_153422-file.mp4"), "20190222_153422")
-
+        self.log_testcase_name(inspect.currentframe().f_code.co_name)
+        self.assertEqual(get_date_from_filename("file-20190222_153422.mp4", datetime_formats['IOS']), "20190222_153422")
+        self.assertEqual(get_date_from_filename("20190222_153422.mp4", datetime_formats['IOS']), "20190222_153422")
+        self.assertEqual(get_date_from_filename("20190222_153422-file.mp4", datetime_formats['IOS']), "20190222_153422")
 
     def test_make_foldername(self):
-        self.assertEqual(make_foldername("20190218_153425", FILENAME_DATETIME, output_formats['MONTHLY']), "2019-02")
-        self.assertEqual(make_foldername("20190218_153425", FILENAME_DATETIME, output_formats['YEARLY']), "2019")
+        self.log_testcase_name(inspect.currentframe().f_code.co_name)
+        self.assertEqual(make_foldername_from_date("20190218_153425", datetime_formats['IOS']['datetime'], output_formats['MONTHLY']),
+                         "2019-02")
+        self.assertEqual(make_foldername_from_date("20190218_153425", datetime_formats['IOS']['datetime'], output_formats['YEARLY']),
+                         "2019")
+        self.assertEqual(make_foldername_from_date("19750517_153425", datetime_formats['IOS']['datetime'], output_formats['DAILY']),
+                         "1975-05-17-Sat")
+
+    # system tests
+    def test_monthly_output(self):
+        self.log_testcase_name(inspect.currentframe().f_code.co_name)
+        testdata.create_test_data()
+        mediasort.scan_files("source", "target", 0, output_formats['MONTHLY'])
+        self.assertTrue(os.path.exists("source"))
+        self.assertTrue(os.path.exists(os.path.join("target", "2019-08")))
+        self.assertTrue(os.path.exists(os.path.join("target", "1975-05", "video")))
+        self.assertTrue(os.path.isfile(os.path.join("target", "1975-05", "video", "file-19750517_091500.mp4")))
+        self.assertTrue(os.path.isfile(os.path.join("target", "2019-08", "IMG_4810.jpeg")))
+        testdata.cleanup_test_data()
+
+    def test_yearly_output(self):
+        self.log_testcase_name(inspect.currentframe().f_code.co_name)
+        testdata.create_test_data()
+        mediasort.scan_files("source", "target", 0, output_formats['YEARLY'])
+        self.assertTrue(os.path.exists(os.path.join("target", "2019")))
+        self.assertTrue(os.path.exists(os.path.join("target", "1975")))
+        self.assertTrue(os.path.isfile(os.path.join("target", "2019", "IMG_4810.jpeg")))
+        testdata.cleanup_test_data()
+
+    def test_daily_output(self):
+        self.log_testcase_name(inspect.currentframe().f_code.co_name)
+        testdata.create_test_data()
+        mediasort.scan_files("source", "target", 0, output_formats['DAILY'])
+        self.assertTrue(os.path.exists(os.path.join("target", "1975-05-17-Sat")))
+        self.assertTrue(os.path.isfile(os.path.join("target", "1975-05-17-Sat", "video", "file-19750517_091500.mp4")))
+        self.assertTrue(os.path.isfile(os.path.join("target", "1975-05-17-Sat", "video", "file-19750517-091500.mp4")))
+        testdata.cleanup_test_data()
+
+    def test_recursion_lvl1(self):
+        self.log_testcase_name(inspect.currentframe().f_code.co_name)
+        testdata.create_test_data()
+        mediasort.scan_files("source", "target", 1, output_formats['YEARLY'])
+        self.assertTrue(os.path.exists(os.path.join("target", "1975")))
+        self.assertTrue(os.path.isfile(os.path.join("target", "1975", "video", "file11-19750517_091500.mp4")))
+        testdata.cleanup_test_data()
+
+    def test_recursion_lvl2(self):
+        self.log_testcase_name(inspect.currentframe().f_code.co_name)
+        testdata.create_test_data()
+        mediasort.scan_files("source", "target", 2, output_formats['YEARLY'])
+        self.assertTrue(os.path.exists(os.path.join("target", "1975")))
+        self.assertTrue(os.path.isfile(os.path.join("target", "1975", "video", "file11-19750517_091500.mp4")))
+        self.assertTrue(os.path.isfile(os.path.join("target", "1975", "video", "file21-19750517_091500.mp4")))
+        testdata.cleanup_test_data()
+
+    def test_recursion_lvl3(self):
+        self.log_testcase_name(inspect.currentframe().f_code.co_name)
+        testdata.create_test_data()
+        mediasort.scan_files("source", "target", 3, output_formats['YEARLY'])
+        self.assertTrue(os.path.exists(os.path.join("target", "1975")))
+        self.assertTrue(os.path.isfile(os.path.join("target", "1975", "video", "file11-19750517_091500.mp4")))
+        self.assertTrue(os.path.isfile(os.path.join("target", "1975", "video", "file21-19750517_091500.mp4")))
+        self.assertTrue(os.path.isfile(os.path.join("target", "1975", "video", "file31-19750517_091500.mp4")))
+        testdata.cleanup_test_data()
 
 
 if __name__ == '__main__':
-    unittest.main()
+    logging.basicConfig(filename='./test_mediasort.log', format='%(asctime)s %(levelname)s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S', filemode='w', level=logging.INFO)
+
+    # unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(MediasortOutput)
+    unittest.TextTestRunner(verbosity=2).run(suite)
